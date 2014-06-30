867743a4f1ed04b140bd588fdca29b5cfe7a4910
Floor5Studio <danitl150@gmail.com>
2014-06-30 15:24:01 -0700

    changes on MouseLook.cs
    MouseLook.cs has changes that make the ship rotate with direction of
    mouse.

--- Scripts/MouseLook.cs
+++ Scripts/MouseLook.cs
@@ -14,32 +14,27 @@ public class MouseLook : MonoBehaviour {
 	public float minimumY = -60F;
 	public float maximumY = 60F;
 
-	public float multiplier = 2F;
+	float rotationY = 0F;
+
+	private Vector2 center;
+	private float MousePos;
 
 	void Update ()
 	{
+		float MousePosX = Input.mousePosition.x;
+		float MousePosY = Input.mousePosition.y;
+
 		if (axes == RotationAxes.MouseXAndY)
 		{
-			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+			float rotationX = transform.localEulerAngles.y + (MousePosX - Screen.width/2) * sensitivityX;
 			
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY += (MousePosY - Screen.height/2) * sensitivityY;
 			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
 			
 			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
 		}
-//		else if (axes == RotationAxes.MouseX)
-//		{
-//			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-//		}
-//		else
-//		{
-//			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-//			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-//			
-//			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-//		}
 	}
-	
+
 	void Start ()
 	{
 		// Make the rigid body not change rotation
