464c25ae424bdb06f4689c95240161f2c2718433
Floor5Studio <danitl150@gmail.com>
2014-06-30 13:17:04 -0700

    Ships motor
    translates the ship forward

---
+++ Scripts/MotorScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotorScript : MonoBehaviour {
+
+	public float speed = 14.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+	void FixedUpdate() {
+
+		transform.Translate(0,0,speed *Time.deltaTime);
+	}
+}
---
+++ Scripts/MouseCursor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseCursor : MonoBehaviour {
+
+	private int cursorWidth = 32;
+	private int cursorHeight = 32;
+	public Texture2D cursorImage;
+	
+	void Start()
+	{
+		Screen.showCursor = false;
+	}
+	
+	
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(Input.mousePosition.x, Screen.height - Input.mousePosition.y, cursorWidth, cursorHeight), cursorImage);
+	}
+}
