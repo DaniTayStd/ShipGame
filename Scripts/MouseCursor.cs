6f1e329c9449a17ad3a16b5832644db166671509
Larry Saeyang <saeyangl@gmail.com>
2014-06-30 19:04:56 -0700

    changed shit

--- Scripts/MouseCursor.cs
+++ Scripts/MouseCursor.cs
@@ -1,20 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MouseCursor : MonoBehaviour {
+public class MouseCursor : MonoBehaviour
+{
 
-	private int cursorWidth = 32;
-	private int cursorHeight = 32;
-	public Texture2D cursorImage;
-	
-	void Start()
-	{
-		Screen.showCursor = false;
-	}
-	
-	
-	void OnGUI()
-	{
-		GUI.DrawTexture(new Rect(Input.mousePosition.x, Screen.height - Input.mousePosition.y, cursorWidth, cursorHeight), cursorImage);
-	}
+    //**************************************************
+    //I switched off the Unity WaterMark because it didn't sit in the middle of the screen so it was a little misleading.
+    //**************************************************
+
+    private int cursorWidth = 32;
+    private int cursorHeight = 32;
+    public Texture2D cursorImage;
+
+    //this is the text for the white reticle, the static one
+    public Texture2D centerText;
+
+    void Start()
+    {
+        Screen.showCursor = false;
+    }
+
+
+    void OnGUI()
+    {
+        //I subtracted half the cursor width and height so the image center sits directly where the mouse pointer is. 
+        GUI.DrawTexture(new Rect(Input.mousePosition.x - cursorWidth / 2, (Screen.height - Input.mousePosition.y) - cursorHeight / 2, cursorWidth, cursorHeight), cursorImage);
+
+        //this is so the white reticle sits directly in the center screen
+        //subtracter half the width and height again for the same reason
+        GUI.DrawTexture(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 200, 200), centerText);
+    }
 }
