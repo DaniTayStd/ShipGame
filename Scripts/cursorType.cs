1e78247900b8293456c3a3a001a296f6599f8c4d
Larry Saeyang <saeyangl@gmail.com>
2014-07-01 23:21:23 -0700

    the mouse enter stuff if on the cursor type script.

--- Scripts/cursorType.cs
+++ Scripts/cursorType.cs
@@ -1,7 +1,14 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class cursorType : MonoBehaviour {
+
+/**********************************************
+Attach this script to a sphere or  some object.
+Make sure the object has a collider on it.
+Then move the mouse to the object and away from it. 
+That's how the functions work.
+************************************************/
 
 	void OnMouseEnter()
 	{
